a8c3fa2fc42bda330586d55656c1789cd64c9051
Chan Yew Yen <chan.yewyen@gmail.com>
2023-03-09 20:00:16 +0800

    First commit

---
+++ src/test/java/com/dxc/demo/loginapp/LoginAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxc.demo.loginapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
